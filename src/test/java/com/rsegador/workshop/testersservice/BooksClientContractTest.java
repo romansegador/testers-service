eda851dd147f3e69ef1f495858ea17ef28e8f202
Roman Segador <romansegador@gmail.com>
2020-12-16 20:10:27 +0100

    Demo

--- src/test/java/com/rsegador/workshop/testersservice/BooksClientContractTest.java
+++ src/test/java/com/rsegador/workshop/testersservice/BooksClientContractTest.java
@@ -44,6 +44,7 @@ public class BooksClientContractTest {
                     .body(PactDslJsonArray
                             .arrayMinLike(1)
                                 .stringType("title", "This is the title of the book")
+                                .stringType("isbn10", "1234567890")
                     )
                 .toPact();
     }
@@ -107,6 +108,7 @@ public class BooksClientContractTest {
     interface TestExpectations {
         Book book = Book.builder()
                 .title("This is the title of the book")
+                .isbn10("1234567890")
                 .build();
     }
 }
